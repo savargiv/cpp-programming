b6c2c4d4505318f1d2bcabe861ecdd19d13341df
savargiv <154443787+savargiv@users.noreply.github.com>
2023-12-22 22:26:59 +0330

    Add files via upload

--- drawing/drawing_1.cpp
+++ drawing/drawing_1.cpp
@@ -6,8 +6,13 @@ int i, j;
 	for(i=1;i<=10;i++) 
 	{
 		for(j=1;j<=10;j++)
+		{
+		if ((i==1)||(i==10)||(j==1)||(j==10))
 			cout << "*";
-		cout << "\n" ;
+		else
+			cout << " ";
+		}
+	cout << "\n" ;
 	}
 return 0;
 }
