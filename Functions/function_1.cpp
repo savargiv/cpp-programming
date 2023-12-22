60809a9d14d114a8df30008e4eb4bf56092fa3c6
savargiv <154443787+savargiv@users.noreply.github.com>
2023-12-22 21:21:24 +0330

    Add files via upload

---
+++ Functions/function_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+/*
+		Print a desired message in the output
+*/
+using namespace std;
+
+void message()
+{
+	cout<<"your arbitrary message... ";
+	cout<<"\n. \n. \n. \n. \n Hello World!!";
+}
+
+int main()
+{ 
+	message();
+
+	getchar();
+	return 0;
+}
+
