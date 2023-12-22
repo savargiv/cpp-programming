28de025a5c6b10530f7a7e1126695623b628911a
savargiv <154443787+savargiv@users.noreply.github.com>
2023-12-22 20:09:24 +0330

    Add files via upload

---
+++ Arrays/simple_string.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+//	Simple String
+	string name, family;
+	cout<<"enter your first name :";
+	cin>>name;
+	
+	cout<<"enter your last name :";
+	cin>>family;
+	
+	cout<<"\n___________________________\n";
+	cout<<"welcome "<<name<<"  "<<family<<endl;
+
+	
+	getchar();
+	return 0;
+}
